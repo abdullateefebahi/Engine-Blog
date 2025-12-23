3e790b0783561c6b3ef78c29d1cfbbdca451ede5
Alabiohio <your@email.com>
2025-12-23 12:40:57 +0100

    updated next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   productionBrowserSourceMaps: false,
+  devIndicators: false,
   images: {
     unoptimized: true,
     remotePatterns: [
